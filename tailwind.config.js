b09a43d797f6f355a5051e857e6ffc72f91f9d2e
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-05-22 10:41:47 -0300

    Aula 12 - Explorando o dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'media',
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
@@ -8,6 +9,7 @@ module.exports = {
           100: '#c542f5',
           200: '#9e35c4',
           300: '#6d2487',
+          400: '#331040'
         }
       }
     }, 
